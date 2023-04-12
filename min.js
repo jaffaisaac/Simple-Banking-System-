0063ea9577db06bfc8bf141a69620a4ff736217a
roy nsumba <jrnsumba@gmail.com>
2023-04-12 19:29:58 +0300

    added transaction history
    logs what a user has done concerning their account.

--- min.js
+++ min.js
@@ -1,26 +1,33 @@
-class BankAccount{
-    constructor(balance){
-        this._balance =balance;
+class BankAccount {
+  constructor(balance) {
+    this._balance = balance;
+    this._transactionHistory = [];
+  }
+  deposit(Amount) {
+    if (Amount > 0) {
+      this._balance += Amount;
+      this._transactionHistory.push(`Deposited ${Amount}`);
     }
-    deposit(Amount){
-        if(Amount > 0){
-            this._balance += Amount;
-        }
+  }
+  withdraw(Amount) {
+    if (Amount > 0 && Amount <= this._balance) {
+      this._balance -= Amount;
+      this._transactionHistory.push(`Withdrew ${Amount}`);
     }
-    withdraw(Amount){
-        if(Amount >0  && Amount <= this._balance){
-            this._balance -= Amount;
-        }
-    }
-    get balance(){
-        return this._balance;
-    }
+  }
+  get balance() {
+    return this._balance;
+  }
+  get transactionHistory() {
+    return this._transactionHistory;
+  }
 }
-let  personX = new BankAccount(5000);
+let personX = new BankAccount(5000);
 
-let  depositcash =personX.deposit(4000);
+personX.deposit(4000);
+console.log(personX.balance);
 personX.withdraw(8000);
-
-console.log(personX.balance)
+console.log(personX.balance);
+console.log(personX.transactionHistory);
 
 //this is the bank  deposite withdraw  and balance check
